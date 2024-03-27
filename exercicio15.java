f3161d094589ef750342c040f0a6cdb9768f72b1
Cauê Gonçalves <cauegon12@gmail.com>
2024-03-26 23:04:24 -0300

    exercicio15

---
+++ exercicio15.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class exercicio15 {
+    public static void executar(){
+
+        Scanner leitor = new Scanner(System.in);
+
+        int[] vetor = new int[5];
+
+        int maior = 0;
+
+        for(int i = 0; i < 5; i++){
+            System.out.printf("Digite o valor da posição %d do vetor: ", i + 1);
+            vetor[i] = leitor.nextInt();
+
+            if(vetor[i] > maior){
+                maior = vetor[i];
+            }
+
+            else{
+                System.out.printf("O numero digitado não é maior que o anterior");
+                break;
+            }
+
+            
+        }
+        
+        
+    }
+}
+
